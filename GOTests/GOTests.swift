448d22d8939a0141c82f2f533d3f40a6918d385f
Srikar <srikarvamsik@gmail.com>
2024-10-22 23:11:12 +0530

    Initial Commit

---
+++ GOTests/GOTests.swift
@@ -0,0 +1,17 @@
+//
+//  GOTests.swift
+//  GOTests
+//
+//  Created by Srikar Vamsi on 22/10/24.
+//
+
+import Testing
+@testable import GO
+
+struct GOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
